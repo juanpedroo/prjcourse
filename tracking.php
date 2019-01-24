d841f685a1f59cb363b7f2092882149949a1f6e8
juanpedroo <jonathan-011@hotmail.fr>
2019-01-24 15:58:05 +0100

    add lib/leaflet workaround tracking.php

--- tracking.php
+++ tracking.php
@@ -1,54 +1,37 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Jekyll v3.8.5">
-    <title>Connection</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page1</title>
+    <link rel="stylesheet" href="lib/leaflet/leaflet.css"/>
 
-    <!-- Bootstrap core CSS -->
-    <link href='https://api.tiles.mapbox.com/mapbox.js/v1.6.2/mapbox.css' rel='stylesheet' />
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.4.0/dist/leaflet.css"
-    integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
-    crossorigin=""/>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <link href="map.css" rel="stylesheet">
-    <!-- Make sure you put this AFTER Leaflet's CSS -->
-    <script src="https://unpkg.com/leaflet@1.4.0/dist/leaflet.js"
-    integrity="sha512-QVftwZFqvtRNi0ZyCtsznlKSWOStnDORoefr1enyq5mVL4tmKB3S/EnC3rRJcxCPavG10IcrVGSmPh6Qw5lwrg=="
-    crossorigin=""></script>
-    <script src='https://api.tiles.mapbox.com/mapbox.js/v1.6.2/mapbox.js'></script>
-    <style>
-    .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-    }
-
-    @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-            font-size: 3.5rem;
-        }
-    }
-    </style>
 </head>
-<body class="text-center">
-<div id='map' style='width: 95%; height: 85%; margin-top:8%;border-style: ridge;'></div>
-<script>
-var map = L.mapbox.map('map')
-            .setView([43.24, 2.1134], 16);
+<body>
 
-        L.control.layers({
-          'Terrain Map': L.mapbox.tileLayer('bobbysud.i2pfp2lb', {
-              detectRetina: true
-            }).addTo(map),
-            'Terrain Map': L.mapbox.tileLayer('bobbysud.i2pfp2lb', {
-                detectRetina: true
-            })
-        }).addTo(map);
+    <div id="mapid" style="width: 700px; height: 800px;"></div>
 
+    <!-- script here -->
+    <script src="lib/leaflet/leaflet.js"></script>
+    <script src="map.js"></script>
+    <script>
+        //Create the map
+        var map = createMap("mapid");
+        var couches = L.layerGroup();
+        //Create the osm layer tiles
+        var osmLayer = createLayer("https://{s}.tile.openstreetmap.org",{});
+        var otmLayer = createLayer("https://{s}.tile.opentopomap.org",{});
+        var hydda = createLayer("https://{s}.tile.openstreetmap.se/hydda/base",{});
 
-</script>
+        var baseMaps = {
+            "OSM" : osmLayer,
+            "OTM" : otmLayer,
+            "Hydda" : hydda
+        };
+        //Add the layer on the map
+        L.control.layers(baseMaps).addTo(map);
+        addOnMap(map,osmLayer);
+    </script>
+
 </body>
 </html>
