038b7d96422ee8b8905bfa398a6b59d3ad8d9ffa
juanpedroo <jonathan-011@hotmail.fr>
2019-05-02 11:41:42 +0200

    tracking v1

--- accueil.php
+++ accueil.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Bootstrap Navbar Dropdown Login and Signup Form with Social Buttons</title>
+<title>SolidariaBram</title>
 <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
