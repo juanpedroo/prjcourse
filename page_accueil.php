9e7cbf0232cc549924c441cfd218c8e540fc7601
juanpedroo <jonathan-011@hotmail.fr>
2019-06-04 16:41:28 +0200

    page_accueil responsive

--- page_accueil.php
+++ page_accueil.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <?php
 	session_start();
 	include 'include/header.inc';
@@ -21,23 +21,16 @@
 		</script>
 	</head>
 	<body>
-
-		<div class="jumbotron" style="height: 60%; width: 40%; margin-left: 10%; float: left; background:transparent !important;">
-			<td valign="center"><img alt="affiche de la course" src="images/affiche_course.jpg" align="center" width="600px" height="780px" >
-		</div>
-
-		<div class="row marketing" style="margin-top: 15%;">
-			<div class="col-lg-12">
-				<h1 class="display-5" style="color: #303030">	Bienvenue dans cette nouvelle édition de Solidària Bram!</h1>
-				<p class="lead" style="color: #585858;"> Nous sommes heureux de vous compter parmis nous.</br> Votre participation permet de financer différentes aides apportées aux enfants démunis.</br> L'association bram solidaire s'occupe de redistribuer l'argent à bon escient.</br> Toutes les actions de l'association sont consultables sur sa page visible en suivant ce lien:</p></p>
-			</p>
-			</div>
-			<br/>
-			<div class="col-lg">
-				<h3>Merci à tous!!
-				<p class="lead" style="color: #585858;">Nous avons récolté 5000 euros depuis la 1ère édition!!
-				</p>
-			</h3>
+		<div class="container">
+			<div class="row marketing" style="margin-top: 5%;">
+				<div class="col-lg-6">
+					<img alt="affiche de la course" src="images/affiche_course.jpg" align="center" width="600px" height="780px" class="img-fluid">
+				</div>
+				<div class="col-lg-6">
+					<h1 class="display-5" style="color: #303030">	Bienvenue dans cette nouvelle édition de Solidària Bram!</h1>
+					<p class="lead" style="color: #585858;"> Nous sommes heureux de vous compter parmis nous.</br> Votre participation permet de financer différentes aides apportées aux enfants démunis.</br> L'association bram solidaire s'occupe de redistribuer l'argent à bon escient.</br> Toutes les actions de l'association sont consultables sur sa page visible en suivant ce lien:</p></p></p>
+					<h3>Merci à tous!!<p class="lead" style="color: #585858;">Nous avons récolté 5000 euros depuis la 1ère édition!!</p></h3>
+				</div>
 			</div>
 		</div>
 	</body>
--- page_association.php
+++ page_association.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <?php
 	session_start();
 	include 'include/header.inc';
